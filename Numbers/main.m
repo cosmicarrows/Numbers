4b6d5a762d7c52cbde56f13951d433f8d8721ac6
Cosmic Arrows <cosmicarrowsllc@Cosmics-MacBook-Pro.local>
2018-05-01 16:30:20 -0400

    Upload assets to Github from hardrive

--- Numbers/main.m
+++ Numbers/main.m
@@ -7,16 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "math.h"
 
 int main(int argc, const char * argv[])
 {
 
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
-    }
+    NSLog(@"3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+    
+    NSLog(@"11 / 3 = %d\n", 11 / 3);
+    
+    NSLog(@"11 / 3.0 = %f\n", 11 / (double)3);
+    
+    NSLog(@"11 / 3 = %d\n", 11 / 3);
+    
+    NSLog(@"11 / 3 = %d remainder of %d\n", 11 / 3, 11 % 3);
+    
+    int x = 5;
+    x--;
+    x/=5;
+    NSLog(@"%d", x);
+    
+    double d;
+    d = sin(1);
+    NSLog(@"%f", d);
     return 0;
 }
 
